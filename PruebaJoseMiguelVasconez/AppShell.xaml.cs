e08ff5e6b959f12b506368968a667348103b932b
laboratorio <laboratorio@UPOAULA10205>
2024-12-02 07:17:01 -0500

    Agregar archivos de proyecto.

---
+++ PruebaJoseMiguelVasconez/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaJoseMiguelVasconez
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PruebaJoseMiguelVasconez/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PruebaJoseMiguelVasconez
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
