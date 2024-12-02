6cbc27d9a437466606855a307c63e3c6e8116a81
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2024-12-02 08:50:37 -0500

    Ser agrego la prfgramacion de la data.

---
+++ PruebaJoseMiguelVasconez/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using PruebaJoseMiguelVasconez.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaJoseMiguelVasconez.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        IEnumerable<Usuario> DevuelveListadoUsuario();
+        Usuario DevuelveUsuario(int telefono);
+        bool CrearUsuario(Usuario usuario);
+        bool ActualizarUsuario(Usuario usuario);
+        bool EliminarUsuario(int telefono);
+    }
+}
---
+++ PruebaJoseMiguelVasconez/Repositories/UsuarioArchivosRepository.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using PruebaJoseMiguelVasconez.Interfaces;
+using PruebaJoseMiguelVasconez.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaJoseMiguelVasconez.Repositories
+{
+    public class UsuarioArchivosRepository : IUsuarioRepository
+    {
+        private string _fileName = Path.Combine(FileSystem.AppDataDirectory, "JoseMIguelVasconez.txt");
+        IEnumerable<Usuario> IUsuarioRepository.DevuelveListadoUsuario()
+        {
+            throw new NotImplementedException();
+        }
+
+        Usuario IUsuarioRepository.DevuelveUsuario(int telefono)
+        {
+            Usuario usuario = new Usuario();
+            if (File.Exists(_fileName))
+            {
+                string json_data = File.ReadAllText(_fileName);
+                usuario = JsonConvert.DeserializeObject<Usuario>(json_data);
+            }
+            return usuario;
+
+        }
+
+        bool IUsuarioRepository.CrearUsuario(Usuario usuario)
+        {
+            try
+            {
+                string json_data = JsonConvert.SerializeObject(usuario);
+                File.WriteAllText(_fileName, json_data);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        bool IUsuarioRepository.ActualizarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IUsuarioRepository.EliminarUsuario(int telefono)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
